efad7985daa7d545dc478d43e35f869dcebc7d2f
Cassidy Ryan Laptop <cassidy.lynn.ryan@gmail.com>
2020-02-11 19:22:16 -0800

    Created basic database table for ingredients with appropriate seed file with some basic entries

---
+++ FODfinder/FODfinder/App_Data/seed.sql
@@ -0,0 +1,19 @@
+﻿INSERT INTO [dbo].[FODMAPIngredients] (Name) VALUES
+	('Agave Syrup'),
+	('Amarinth Flour'),
+	('Barley'),
+	('Buttermilk'),
+	('Einkorn'),
+	('Emmer'),
+	('Fructose'),
+	('Fruit Concentrate'),
+	('Garlic'),
+	('Glucose-fructose syrup')
+GO
+
+INSERT INTO [dbo].[FODMAPIngredients] (Name, Aliases) VALUES
+	('Chickory Root', 'Chickory Extract'),
+	('Chickpea Flour', 'Besan'),
+	('Erythritol', 'E968'),
+	('Fructooligosaccharides', 'FOS')
+GO
