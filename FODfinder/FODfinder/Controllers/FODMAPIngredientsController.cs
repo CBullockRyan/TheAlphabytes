ac3105c181652f6b34d32085e2625f55fc42f401
Cassidy Ryan Laptop <cassidy.lynn.ryan@gmail.com>
2020-02-12 11:41:59 -0800

    Created a search for ingredients, next step is to add the new pages to the nav bar. The FODMAP info page link also needs to be added to the search page. The database needs to be made more comprehensive and referenced with the USDA API

--- FODfinder/FODfinder/Controllers/FODMAPIngredientsController.cs
+++ FODfinder/FODfinder/Controllers/FODMAPIngredientsController.cs
@@ -14,105 +14,27 @@ namespace FODfinder.Controllers
     {
         private FFDBContext db = new FFDBContext();
 
-        // GET: FODMAPIngredients
         public ActionResult Browse()
         {
             return View(db.FODMAPIngredients.ToList());
         }
 
-        // GET: FODMAPIngredients/Details/5
-        public ActionResult Details(int? id)
+        [HttpGet]
+        public ActionResult Search()
         {
-            if (id == null)
+            string search = Request.QueryString["search"];
+            if (!String.IsNullOrEmpty(search))
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                var matchAlias = db.FODMAPIngredients.Where(p => p.Aliases.Contains(search));
+                List<FODMAPIngredient> allMatches = db.FODMAPIngredients.Where(p => p.Name.Contains(search)).Union(matchAlias).ToList();
+                ViewBag.Success = true;
+                if (!allMatches.Any())
+                {
+                    ViewBag.Success = false;
+                }
+                return View(allMatches);
             }
-            FODMAPIngredient fODMAPIngredient = db.FODMAPIngredients.Find(id);
-            if (fODMAPIngredient == null)
-            {
-                return HttpNotFound();
-            }
-            return View(fODMAPIngredient);
-        }
-
-        // GET: FODMAPIngredients/Create
-        public ActionResult Create()
-        {
             return View();
-        }
-
-        // POST: FODMAPIngredients/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Name,Aliases")] FODMAPIngredient fODMAPIngredient)
-        {
-            if (ModelState.IsValid)
-            {
-                db.FODMAPIngredients.Add(fODMAPIngredient);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(fODMAPIngredient);
-        }
-
-        // GET: FODMAPIngredients/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            FODMAPIngredient fODMAPIngredient = db.FODMAPIngredients.Find(id);
-            if (fODMAPIngredient == null)
-            {
-                return HttpNotFound();
-            }
-            return View(fODMAPIngredient);
-        }
-
-        // POST: FODMAPIngredients/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Name,Aliases")] FODMAPIngredient fODMAPIngredient)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(fODMAPIngredient).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(fODMAPIngredient);
-        }
-
-        // GET: FODMAPIngredients/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            FODMAPIngredient fODMAPIngredient = db.FODMAPIngredients.Find(id);
-            if (fODMAPIngredient == null)
-            {
-                return HttpNotFound();
-            }
-            return View(fODMAPIngredient);
-        }
-
-        // POST: FODMAPIngredients/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            FODMAPIngredient fODMAPIngredient = db.FODMAPIngredients.Find(id);
-            db.FODMAPIngredients.Remove(fODMAPIngredient);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
