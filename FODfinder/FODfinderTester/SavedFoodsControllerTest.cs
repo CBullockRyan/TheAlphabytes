1dd204e9f9d701bd5b84e5c1519e8c9954cbfe55
Cassidy Ryan Desktop <cbullock18@wou.edu>
2020-04-11 18:21:57 -0700

    refactored for test, but environment is having problems

---
+++ FODfinder/FODfinderTester/SavedFoodsControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FODfinder;
+using FODfinder.Controllers;
+using FODfinder.Models.SavedFood;
+
+namespace FODfinderTester
+{
+    [TestClass]
+    public class SavedFoodsControllerTest
+    {
+        
+
+        [TestMethod]
+        public void TestCanCount_Regular()
+        {
+            //Arrange
+            SavedFoodsController c = new SavedFoodsController();
+            //Act
+            List<SavedFood> testList = new List<SavedFood>();
+            int testCount = c.countSavedFoods(testList);
+            int expectedCount = 3;
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestCanCount_EmptyList()
+        {
+
+        }
+    }
+}
